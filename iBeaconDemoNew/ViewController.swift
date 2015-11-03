ca2b66b4f61dd00c2c0e9745ded01499f114b9ab
田栗信昭 <taguri@gicon.jp>
2015-11-03 12:15:15 +0900

    Initial Commit

---
+++ iBeaconDemoNew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iBeaconDemoNew
+//
+//  Created by 田栗信昭 on 2015/11/03.
+//  Copyright © 2015年 田栗信昭. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
